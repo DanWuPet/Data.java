02df77e056ff3a51ae7381ae0fafd74a80606f0b
DanWuPet <89942751+DanWuPet@users.noreply.github.com>
2021-09-04 14:48:45 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        //Variables
         String day = "Thursday";
         int date = 16;
         String month = "July ";
         int year = 2015;
-
+        
+        //Outputs
         System.out.println("American format:");
         System.out.println(day + ", " + month + date + ", " + year);
         System.out.println("European format:");
